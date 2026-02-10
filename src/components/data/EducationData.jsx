d4034dc1caa053e3d7e7e2a433b6cb2e60bab57e
giulia <giuliamoukouyou@gmail.com>
2026-02-10 13:09:06 +0100

    Edit: edit text

--- src/components/data/EducationData.jsx
+++ src/components/data/EducationData.jsx
@@ -1,48 +1,44 @@
 export const EducationData = [
-    {
-        title: "Laurea Magistrale",
-        school: "Università degli Studi di Milano",
-        date: "09/2023 - in corso",
-        description:"Corso di laurea magistrale in Filologia, letterature e storia dell'antichità.",
-        certificateSrc: null,
-        certificateLink: null,
-        alt:null
-    }, 
-    {
-        title: "Laurea Triennale in Lettere",
-        school: "Università degli Studi di Milano",
-        date: "09/2016 - 07/2021",
-        description:"Corso di laurea in ambito umanistico, incentrato sullo sviluppo di soft skills come comunicazione, scrittura e ascolto.",
-        certificateSrc: null,
-        certificateLink: null,
-        alt:null
-    },
-    {
-        title: "Corsi di sviluppo front-end e back-end",
-        school: "Codegrind",
-        date: "2025",
-        description:`Ho seguito i corsi su JavaScript avanzato, Node.js e React della piattaforma Codegrind.`,
-        certificateSrc: "",
-        certificateLink:"",
-        alt: ""
-    },
-    {
-        title: "Content-driven web application foundation",
-        school: "Sanity Learn",
-        date: "2024",
-        description:`Sviluppo di un blog full-stack con Next.js e Sanity CMS. 
+  {
+    title: "Laurea Triennale in Lettere",
+    school: "Università degli Studi di Milano",
+    date: "09/2016 - 07/2021",
+    description:
+      "Percorso di laurea con orientamento a studi ecologici, con tesi in Ecologia preistorica, basata su analisi e interpretazione di dati ambientali.",
+    certificateSrc: null,
+    certificateLink: null,
+    alt: null,
+  },
+  {
+    title: "Corsi di sviluppo front-end e back-end",
+    school: "Codegrind",
+    date: "2025",
+    description: `Ho seguito i corsi su JavaScript avanzato, Node.js e React della piattaforma Codegrind.`,
+    certificateSrc: "",
+    certificateLink: "",
+    alt: "",
+  },
+  {
+    title: "Content-driven web application foundation",
+    school: "Sanity Learn",
+    date: "2024",
+    description: `Sviluppo di un blog full-stack con Next.js e Sanity CMS. 
             Ho approfondito l’integrazione dei dati tramite GraphQL e la gestione dei contenuti con un approccio headless.`,
-        certificateSrc: "./certificate-content-driven-web-application-foundations.png",
-        certificateLink:"https://www.sanity.io/learn/course/content-driven-web-application-foundations/share/gfwE6oY85?utm_campaign=course_completed&utm_medium=social&utm_source=copy_link",
-        alt: "Certificato del corso Content-driven web application foundation"
-    },
-    {
-        title: "Introduction to Front-End development",
-        school: "Coursera",
-        date: "2024",
-        description:"Introduzione allo sviluppo web: HTML, CSS, Bootstrap, JavaScript e responsive design.",
-        certificateSrc: "./meta.png",
-        certificateLink: "https://www.coursera.org/account/accomplishments/verify/SGADLMM2PX2D",
-        alt:"Certificato del corso Introduction to Front-End development"
-    }
-]
+    certificateSrc:
+      "./certificate-content-driven-web-application-foundations.png",
+    certificateLink:
+      "https://www.sanity.io/learn/course/content-driven-web-application-foundations/share/gfwE6oY85?utm_campaign=course_completed&utm_medium=social&utm_source=copy_link",
+    alt: "Certificato del corso Content-driven web application foundation",
+  },
+  {
+    title: "Introduction to Front-End development",
+    school: "Coursera",
+    date: "2024",
+    description:
+      "Introduzione allo sviluppo web: HTML, CSS, Bootstrap, JavaScript e responsive design.",
+    certificateSrc: "./meta.png",
+    certificateLink:
+      "https://www.coursera.org/account/accomplishments/verify/SGADLMM2PX2D",
+    alt: "Certificato del corso Introduction to Front-End development",
+  },
+];
